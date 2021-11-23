fb651959f38040a3b77b49f5da786c6b54ab8ebe
avinash <biradarav.100@gmail.com>
2021-11-23 12:50:58 +0530

    updated UI and improve accuracy

--- app.py
+++ app.py
@@ -2,46 +2,50 @@ import streamlit as st
 import pickle
 import numpy as np
 
+# page title
+
+st.set_page_config(page_title="Laptop Price Predictor",page_icon="💻")
+
 # import the model
 pipe = pickle.load(open('pipe.pkl','rb'))
 df = pickle.load(open('df.pkl','rb'))
 
-st.title("Laptop Predictor")
-
+st.title("Laptop Price Predictor")
+st.sidebar.title("Enter specification")
 # brand
-company = st.selectbox('Brand',df['Company'].unique())
+company = st.sidebar.selectbox('Brand',df['Company'].unique())
 
 # type of laptop
-type = st.selectbox('Type',df['TypeName'].unique())
+type = st.sidebar.selectbox('Type',df['TypeName'].unique())
 
 # Ram
-ram = st.selectbox('RAM(in GB)',df['Ram'].unique())
+ram = st.sidebar.selectbox('RAM(in GB)',df['Ram'].unique())
 
 # weight
-weight = st.selectbox('Weight of the Laptop(kg)',[1.0,1.5,1.75,2.0,2.5,3,3.5])
+weight = st.sidebar.selectbox('Weight of the Laptop(kg)',[1.0,1.5,1.75,2.0,2.5,3,3.5])
 
 # Touchscreen
-touchscreen = st.selectbox('Touchscreen',['No','Yes'])
+touchscreen = st.sidebar.selectbox('Touchscreen',['No','Yes'])
 
 # IPS
-ips = st.selectbox('IPS',['No','Yes'])
+ips = st.sidebar.selectbox('IPS',['No','Yes'])
 
 # screen size
-screen_size = st.selectbox('Screen Size',[14,14.5,15,15.6,16,17,17.3])
+screen_size = st.sidebar.selectbox('Screen Size',[14,14.5,15,15.6,16,17,17.3])
 
 # resolution
-resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
+resolution = st.sidebar.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
 
 #cpu
-cpu = st.selectbox('CPU',df['Cpu brand'].unique())
+cpu = st.sidebar.selectbox('CPU',df['Cpu brand'].unique())
 
-hdd = st.selectbox('HDD(in GB)',[500,1000,2000])
+hdd = st.sidebar.selectbox('HDD(in GB)',[500,1000,2000])
 
-ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+ssd = st.sidebar.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
 
-gpu = st.selectbox('GPU',df['Gpu brand'].unique())
+gpu = st.sidebar.selectbox('GPU',df['Gpu brand'].unique())
 
-os = st.selectbox('OS',df['os'].unique())
+os = st.sidebar.selectbox('OS',df['os'].unique())
 
 if st.button('Predict Price'):
     # query
